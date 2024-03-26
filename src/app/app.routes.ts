dc58fd05c6078be582c21d58588926f93575dd31
Aayush Kumar <75976366+aayushkumar4u17@users.noreply.github.com>
2024-03-26 14:48:28 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LiveAnalysisComponent } from './live-analysis/live-analysis.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path:'', redirectTo: 'home', pathMatch: 'full' },
+    { path:'home', component: HomeComponent},
+    { path:'live-analysis', component:LiveAnalysisComponent},
+    { path:'blog', component: BlogComponent}
+];
