6bc48d0cf201984e22b75cb9f94d388d44447aca
danishkha007 <khand000786@gmail.com>
2024-06-15 18:38:12 +0530

    new-changes blog and calc

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { howToUse } from "../components/blog-cards/blog-card.component";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
+  imports: [howToUse]
 })
 export class BlogComponent {
 
